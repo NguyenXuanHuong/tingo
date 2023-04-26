d4f47083667dd5842e9cf9d9993e066cabb71144
BTTTrang <thithutrang.bui@bluebik.com>
2023-04-26 13:25:28 +0700

    ab

--- test-image/src/main/java/com/example/testimage/controller/TestController.java
+++ test-image/src/main/java/com/example/testimage/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("test")
     private String test1() {
-        return "success";
+        return "success1";
     }
 }
