a26cbba897c6a692779e2de27f52bd0eb3f0d181
BTTTrang <thithutrang.bui@bluebik.com>
2023-04-26 12:57:04 +0700

    ad

---
+++ test-image/src/main/java/com/example/testimage/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.testimage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class TestController {
+    @GetMapping("test")
+    private String test1() {
+        return "success";
+    }
+}
